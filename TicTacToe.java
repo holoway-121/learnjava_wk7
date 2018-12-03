5cb935b2d033a04cd31bf6f7fd222b4ac098e981
David Sam <David.Sam@northampton.ac.uk>
2018-12-03 20:36:26 +0000

    Updating files on master

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,33 @@
 package tictactoe;
 
-public class TicTacToe {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class TicTacToe extends Application{
+
+public void start(Stage stage) throws Exception {
+		
+		// Load the FXML file.
+	      Parent parent = FXMLLoader.load(
+	               getClass().getResource("tictactoe.fxml")); 
+	      
+	      // Build the scene graph.
+	      Scene scene = new Scene(parent); 
+	      
+	      // Display our window, using the scene graph.
+	      stage.setTitle("TicTacToe"); 
+	      stage.setScene(scene);
+	      stage.show(); 
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
-		System.out.println("Test");
+		launch(args);
 	}
 
 }
---
+++ TicTacToeController.java
@@ -0,0 +1,160 @@
+package tictactoe;
+
+import java.util.Random;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class TicTacToeController {
+
+	
+	@FXML
+    private ImageView imgView1;
+
+    @FXML
+    private ImageView imgView2;
+
+    @FXML
+    private ImageView imgView3;
+
+    @FXML
+    private ImageView imgView4;
+
+    @FXML
+    private ImageView imgView6;
+
+    @FXML
+    private ImageView imgView5;
+
+    @FXML
+    private ImageView imgView9;
+
+    @FXML
+    private ImageView imgView8;
+
+    @FXML
+    private ImageView imgView7;
+
+    @FXML
+    private Button startBtn;
+    
+    @FXML
+    private RadioButton xRdioBtn;
+
+    @FXML
+    private RadioButton oRdioBtn;
+    
+    private Image oImg = new Image("\\wk7\\tictactoe_o.jpg");
+    private Image xImg = new Image("\\wk7\\tictactoe_x.jpg");
+    
+    //private Image[][] gBoard = new Image[3][3];
+    /*
+    {00,01,02},
+    {10,11,12},
+    {20,21,22}
+    */
+    
+    private int userChoice;
+    
+    private Image userPickedImg;
+    Random rand = new Random();
+    
+    //Game
+    public void start() {
+    	if(xRdioBtn.isSelected()) {
+    		userChoice = 0;
+    		userPickedImg = xImg;
+    	}
+    	else {
+    		userChoice = 1;
+    		userPickedImg = oImg;
+    	}
+    }
+    
+    public void userPlay() {
+    	if(userChoice == 0) {
+    		userPickedImg = oImg;
+    		userChoice = 1;
+    		
+    	}
+    	else if(userChoice == 1) {
+    		userPickedImg = xImg;
+    		userChoice = 0;
+    	}
+    	
+    }
+    
+    //Play against comp? auto generate numb, check its not set, then play?
+    public void compPlay() {
+    	int compPick = rand.nextInt(9);
+    	
+    	
+    }
+    
+    
+    public void setImgView1() {
+    	if(imgView1.getImage() == null) {
+    		imgView1.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView2() {
+    	if(imgView2.getImage() == null) {
+    		imgView2.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+
+    public void setImgView3() {
+    	if(imgView3.getImage() == null) {
+    		imgView3.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView4() {
+    	if(imgView4.getImage() == null) {
+    		imgView4.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView5() {
+    	if(imgView5.getImage() == null) {
+    		imgView5.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView6() {
+    	if(imgView6.getImage() == null) {
+    		imgView6.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView7() {
+    	if(imgView7.getImage() == null) {
+    		imgView7.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView8() {
+    	if(imgView8.getImage() == null) {
+    		imgView8.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+    
+    public void setImgView9() {
+    	if(imgView9.getImage() == null) {
+    		imgView9.setImage(userPickedImg);
+    		userPlay();
+    	}
+    }
+}
